c290a21827ea4af9ba32d0f6b867f96aef452e05
Joel Del Cueto <wayfaringjou@gmail.com>
2021-01-19 14:00:14 -0500

    Add basic express and knex set-up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
+const knex = require('knex');
 const pg = require('pg');
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 
-pg.defaults.ssl = {
-  rejectUnauthorized: false,
-};
+// pg.defaults.ssl = {
+//  rejectUnauthorized: false,
+// };
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+  // ssl: {
+  //  rejectUnauthorized: false,
+  // },
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
